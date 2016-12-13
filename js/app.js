b9776853c39219930c2315909ef4e3960d033d6b
eeooee <epiphany.compton@gmail.com>
2016-12-12 18:13:45 -0600

    working search functionality, dynamic card display working

--- js/app.js
+++ js/app.js
@@ -19,20 +19,39 @@ var getItunesUrl = function(input){
  return itunesUrl.concat(searchString);
 }
 
-var formatResults = function(searchResult){
-var content = searchResult.trackName + "-";
-content += searchResult.artistName;
+var formatResults = function(searchResult, rowID){
+ 
+let content = '<div class="col-sm-3" id="searchResults">';
+content += '<div class="card">';
+content += '<div class = "card-block">';
+content += '<img src="'+searchResult.artworkUrl100+'" class="img-fluid" id = "albumArt" alt="Responsive image">'
+content += '<p>' + searchResult.trackName + " - ";
+content += searchResult.artistName + '</p>';
+content +='<audio class="sample" controls preload="none">';
+content += '<source src="'+ searchResult.previewUrl + '"type="audio/m4a">';
+content += '</audio>'
+content += '</div>';
+content += '</div>';
+content += '</div>';
 console.log(content);
-return content;
+$('#row'+rowID).append(content);
 }
+
+
 
 var searchItunes = function(input){
     $.ajax({
         url: input,
         dataType: "JSONP",
         success:function(data){
-            for(let i=0; i<5; i++){
-                formatResults(data.results[i]);
+           let id = 0;
+            for(let i=0; i<20; i++){
+                    if((i%4==0)||i==0){
+                        id++;
+                        let newRow = '<div class = "row" id=row'+id+'> </div>';
+                        $('#results').append(newRow);
+                    }
+                    formatResults(data.results[i],id);
             }
         }
         
